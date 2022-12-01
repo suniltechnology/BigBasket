277ea10fe9af6c198184b7b939b4fe2f303fa1eb
sunilmargale <sunilmargale27@gmail.com>
2022-12-01 09:58:45 +0000

    last change

--- payment.sh
+++ payment.sh
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 payment gateway
+=======
+!#/bin/bash
+PG=ICICI
+IFSC=012767543
+AC NO=55412451245
+
+echo "my acc no is ${AC No}"
+>>>>>>> parent of c817c7a (Upadating ..Revert "Adding Payment Script")
